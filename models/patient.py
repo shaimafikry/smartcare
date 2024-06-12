6e2240198a9badbed87e716555576a3d6c45c991
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2024-06-13 02:07:43 +0300

    Update patient.py

--- models/patient.py
+++ models/patient.py
@@ -1,6 +1,33 @@
 #!/usr/bin/python3
 """main module for base class
     """
-
-import uuid
+import random
+import string
 from datetime import datetime
+
+def generate_id(number):
+    random.seed(number)
+    letter = random.choice(string.ascii_uppercase)
+    digits = ''.join(random.choices(string.digits, k=9))
+    unique_id = letter + digits
+    return unique_id
+
+class Patient:
+""" patient profile"""
+def __init__(self,*args,*kwargs):
+self.name = ""
+self.adress = ""
+self.phone = ""
+self.nat_id = ""
+self.id = generate_id(slef.nat_id)
+self.sex = ""
+self.age = ""
+self.history = ""
+self.treatment = ""
+self.complain = ""
+self.scans = ""
+self.labs= ""
+self.admission_date = date.time(now)
+self.discharge_date = ""
+self.update = ""
+self.vitals = ""
