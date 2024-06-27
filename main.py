9072cd409c30fdbc3d75ce0ea7cd3fcfdaf18a3a
shaimaa <shaimafikry@yahoo.com>
2024-06-27 23:15:09 +0300

    edit sql db for creating more than one object at the same time

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 from models import storage
 from models.patient import Patient
 from models.doctor import Doctor
+from models.manager import Manager
 all_objs = storage.all()
 print("-- Reloaded objects --")
 for obj_id in all_objs.keys():
@@ -17,6 +18,19 @@ print(my_model)
 
 print("-- Create a 2 object --")
 my_model = Patient(29709151313565)
+my_model.name = "My_First_Model"
+my_model.my_number = 89
+my_model.save()
+print(my_model)
+
+print("-- Create a 3 object --")
+my_model = Manager()
+my_model.name = "My_First_Model"
+my_model.my_number = 89
+my_model.save()
+print(my_model)
+print("-- Create a 4 object --")
+my_model = Manager()
 my_model.name = "My_First_Model"
 my_model.my_number = 89
 my_model.save()
--- models/base_model.py
+++ models/base_model.py
@@ -8,33 +8,38 @@ from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
 from sqlalchemy.orm import Session
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.event import listen
+from sqlalchemy.exc import IntegrityError
 
 Base = declarative_base()
 
 class BaseModel():
     """ main class to generate id"""
+    __id = 0
+    def generate_id (class_type):
+        BaseModel.__id += 1
+        id = BaseModel.__id
+        # zero fills to generate id with leading zeros
+        id = str(id).zfill(5)
+        if class_type == 'Doctor':
+            id = 'D' + id
+        if class_type == 'Manager':
+            id = 'M' + id
+        if class_type == 'Receptionist':
+            id = 'R' + id
+        if class_type == 'Nurse':
+            id = 'N' + id
+        return id
+    
     if os.getenv('TYPE_STORAGE') == "db":
         id = Column(String(60), nullable=False, primary_key=True)
         join_at = Column(DateTime, nullable=False, default=datetime.utcnow())
         edit_at = Column(DateTime, nullable=False, default=datetime.utcnow())
-    __id = 0
+
+
     def __init__(self, class_type=None, *args, **kwargs):
         # print(f"class_type: {class_type}")  # Debugging output
         if not kwargs:
-            BaseModel.__id += 1
-            id = BaseModel.__id
-            # zero fills to generate id with leading zeros
-            id = str(id).zfill(5)
-            if class_type == 'Doctor':
-                    self.id = 'D' + id
-            elif class_type == 'Manager':
-                self.id = 'M' + id
-            elif class_type == 'Receptionist':
-                    self.id = 'R' + id
-            elif class_type == 'Nurse':
-                    self.id = 'N' + id
-            else:
-                self.id = 'V' + id
+            self.id = BaseModel.generate_id(class_type)
             # date of join
             self.join_at = datetime.now()
             # date of edit
@@ -42,37 +47,15 @@ class BaseModel():
             from models import storage
             storage.new(self)
         else:
-            if kwargs.get('id') == None:
-                BaseModel.__id += 1
-                id = BaseModel.__id
-                # zero fills to generate id with leading zeros
-                id = str(id).zfill(5)
-                if class_type == 'Doctor':
-                        self.id = 'D' + id
-                elif class_type == 'Manager':
-                    self.id = 'M' + id
-                elif class_type == 'Receptionist':
-                        self.id = 'R' + id
-                elif class_type == 'Nurse':
-                        self.id = 'N' + id
-                else:
-                    self.id = 'V' + id
-            else:
-                self.id = kwargs['id']
-            # if kwargs.get('join_date') is None:
-            #     self.join_at = datetime.now()
-            # else:
+            self.id = kwargs.get('id', BaseModel.generate_id(class_type))
             kwargs['join_at'] = datetime.strptime(kwargs['join_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
-            #should "edit at" ony be mdoified after reload?
-            # if kwargs.get('edit_at') is None:
-            #     self.edit_at = datetime.now()
-            # else:
             kwargs['edit_at'] = datetime.strptime(kwargs['edit_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
             if kwargs.get('__class__') != None:
                 del kwargs['__class__']
             self.__dict__.update(kwargs)
+
 
     def save (self):
         """save to file_storage.__workers"""
@@ -80,6 +63,7 @@ class BaseModel():
         self.edit_at = datetime.now()
         storage.new(self)
         storage.save()
+
 
     def __str__(self):
         """Returns a string representation of the instance"""
