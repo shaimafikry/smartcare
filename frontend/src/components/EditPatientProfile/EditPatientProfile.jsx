8008920556953804e71610348307635525369d3c
saraa93-bit <saraa93177228@gmail.com>
2024-10-20 23:58:38 +0300

    app  editting

--- frontend/src/components/EditPatientProfile/EditPatientProfile.jsx
+++ frontend/src/components/EditPatientProfile/EditPatientProfile.jsx
@@ -6,7 +6,7 @@ import { useNavigate, useParams } from 'react-router-dom'; // Import useNavigate
 
 
 function EditProfile({ message }) {
-	const navigate = useNavigate(); // Get the navigate function
+	const navigate = useNavigate();
 	const { id } = useParams();
 	const [formData, setFormData] = useState({
     complain: '',
