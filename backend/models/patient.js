488e0dd2a6157d7cf9b46540c1c7d4dfc6df50f4
foash-111 <mr2221@fayoum.edu.eg>
2024-10-23 12:40:45 +0300

    patient related queries methods

--- backend/models/patient.js
+++ backend/models/patient.js
@@ -1,201 +1,165 @@
-
 // patient module
 const { Patient, PatientDetails } = require('../config/patients');
-const { Op } = require('sequelize'); // Make sure to import Op for Sequelize operators
+const { Op } = require('sequelize'); // Import Op for Sequelize operators
 
-
-// Mark:receptionist_add
+// Mark: receptionist_add
 async function addPatient(patient) {
-
     try {
-
-			const newPatient = await Patient.create(patient); 
+        const newPatient = await Patient.create(patient);
         return newPatient.get({ plain: true });
     } catch (error) {
         throw new Error(`Error adding patient: ${error.message}`);
     }
 }
 
-
-
-// Mark: receptionist edit
+// Mark: receptionist_edit
 async function editPatient(id, updatedData) {
-
     try {
         const patient = await Patient.findByPk(id);
         if (!patient) {
             throw new Error('Patient not found');
         }
 
-        await Patient.update(updatedData, { where: { id: id } });
+        await Patient.update(updatedData, { where: { id } });
         return true;
     } catch (error) {
         throw new Error(`Error updating patient: ${error.message}`);
     }
 }
 
-
-
-// make status 'out' when discharge patient
+// Mark: discharge patient
 async function dischargePatient(id) {
-    
-        await Patient.update({ status: 'out' }, { where: { id: id } });
-        return true;
+    await Patient.update({ status: 'out' }, { where: { id } });
+    return true;
 }
 
-
-
+// Function to find a patient
 async function findPatient(searchInput) {
     try {
-				 // Determine if the searchInput is a valid integer (for id) or string (for name)
-				 const isNumber = !isNaN(searchInput);
+        // Determine if searchInput is a valid integer (for id) or string (for name)
+        const isNumber = !isNaN(searchInput);
 
-				 // Construct the where clause based on whether searchInput is a number or a string
-				 const whereClause = isNumber
-						 ? { id: parseInt(searchInput, 10) } // Search by ID if it's a number
-						 : {
-									 // Otherwise, search by name using case-insensitive matching
-									 name: {
-											 [Op.iLike]: `%${searchInput}%`
-									 }
-							 };
- 
+        // Construct the where clause based on the type of searchInput
+        const whereClause = isNumber
+            ? { id: parseInt(searchInput, 10) } // Search by ID if it's a number
+            : {
+                name: {
+                    [Op.iLike]: `%${searchInput}%`, // Case-insensitive matching
+                },
+            };
 
         const patient = await Patient.findOne({
             where: whereClause,
             include: [PatientDetails], // Include related details
         });
-        // console.log("patient module",patient);
-        return patient.get({ plain: true }); // Return plain objects of the patients
+
+        return patient ? patient.get({ plain: true }) : null; // Return plain object or null
     } catch (error) {
         throw new Error(`Error finding patients: ${error.message}`);
     }
 }
 
-
-
-// receptioinst search to add 
+// Receptionist search to add
 async function receptionistFindOnePatient(national_id) {
     try {
-        // Constructing the where clause to search by either national_id or name
         const patient = await Patient.findOne({
-            where:{ national_id: national_id},
+            where: { national_id },
         });
 
-				if (!patient) {
-					return null; // Or throw an error
-			}
-        return patient.get({ plain: true }); // Return plain objects of the patients
+        return patient ? patient.get({ plain: true }) : null; // Return plain object or null
+    } catch (error) {
+        throw new Error(`Error finding patient: ${error.message}`);
+    }
+}
+
+// Receptionist search
+async function receptionistFindPatient(searchInput) {
+    try {
+        const whereClause = {
+            [Op.or]: [
+                { national_id: searchInput }, // Search by national_id
+                {
+                    name: {
+                        [Op.iLike]: `%${searchInput}%`, // Case-insensitive name search
+                    },
+                },
+            ],
+        };
+
+        const patients = await Patient.findAll({ where: whereClause });
+
+        return patients.map(patient => patient.get({ plain: true })); // Return plain objects
     } catch (error) {
         throw new Error(`Error finding patients: ${error.message}`);
     }
 }
 
-
-// receptioinst search
-async function receptionistFindPatient(searchInput) {
-	try {
-			// Constructing the where clause to search by either national_id or name
-			const whereClause = {
-					[Op.or]: [
-							{
-									national_id: searchInput // Search by national_id
-							},
-							{
-									name: {
-											[Op.iLike]: `%${searchInput}%` // Search by name with case-insensitive matching
-									}
-							}
-					]
-			};
-
-			const patients = await Patient.findAll({
-					where: whereClause,
-			});
-
-			return patients.map(patient => patient.get({ plain: true })); // Return plain objects of the patients
-	} catch (error) {
-			throw new Error(`Error finding patients: ${error.message}`);
-	}
-}
-
-
-
-//patientDetail Table
-
+// Edit patient medical records
 async function editPatientMedical(id, updatedData) {
     try {
-			  const { discharge, ...Data } = updatedData; // Destructure the discharge flag from the body
+        const { discharge, ...data } = updatedData; // Destructure discharge flag
 
-			  // check if it has past records
-				const patientMedRecord = await PatientDetails.findOne({ where: { patient_id: id } });
-				if (patientMedRecord) {
+        // Check if there are past records
+        const patientMedRecord = await PatientDetails.findOne({ where: { patient_id: id } });
+        if (patientMedRecord) {
+            await PatientDetails.update(data, { where: { patient_id: id } });
+        } else {
+            await PatientDetails.create({ patient_id: id, ...data });
+        }
 
-					await PatientDetails.update(Data, { where: { patient_id: id } });
-				}
-				 
-        else {
-					await PatientDetails.create({ patient_id: id, ...Data });
-				}
-				if (discharge){
-					await dischargePatient(id);
-				}
-				return true;
+        if (discharge) {
+            await dischargePatient(id);
+        }
+
+        return true;
     } catch (error) {
         throw new Error(`Error updating patient: ${error.message}`);
     }
 }
 
-
-
+// Get patients in department
 async function getPatientsInDepartment(departmentName) {
     try {
         const patients = await Patient.findAll({
             where: {
-                department: departmentName, // Replace with actual department
-                status: ['new', 'resident'] // Status should be either 'new' or 'resident'
+                department: departmentName, // Filter by department
+                status: ['new', 'resident'], // Status should be either 'new' or 'resident'
             },
-						include: [PatientDetails], // to include ralational Patient details
-            order: [
-                ['createdAt', 'DESC'] // Sort by createdAt in descending order
-            ]
+            include: [PatientDetails], // Include relational Patient details
+            order: [['createdAt', 'DESC']], // Sort by createdAt in descending order
         });
-        // Check if patients array is empty
-        if (patients.length === 0) {
-            return null;
-        }
 
-        return patients; // Return the array of patients
+        return patients.length ? patients : null; // Return patients or null
     } catch (error) {
         console.error(`Error fetching patients: ${error.message}`);
         throw error; // Handle the error
     }
 }
 
-
-
-
+// Get all patients
 async function getAllPatients() {
-	try {
-		const patients = await Patient.findAll({
-			include: [PatientDetails], // to include ralational Patient details
-			order: [
-					['createdAt', 'DESC'] // Sort by createdAt in descending order
-			]
-	});
-			// Check if patients array is empty
-			if (patients.length === 0) {
-					return null;
-			}
+    try {
+        const patients = await Patient.findAll({
+            include: [PatientDetails], // Include relational Patient details
+            order: [['createdAt', 'DESC']], // Sort by createdAt in descending order
+        });
 
-			return patients; // Return the array of patients
-	} catch (error) {
-			console.error(`Error fetching patients: ${error.message}`);
-			throw error; // Handle the error
-	}
+        return patients.length ? patients : null; // Return patients or null
+    } catch (error) {
+        console.error(`Error fetching patients: ${error.message}`);
+        throw error; // Handle the error
+    }
 }
 
-
-
-
-module.exports = { addPatient, editPatient, findPatient, receptionistFindPatient, dischargePatient, editPatientMedical, getPatientsInDepartment, receptionistFindOnePatient, getAllPatients };
+// Export functions
+module.exports = {
+    addPatient,
+    editPatient,
+    findPatient,
+    receptionistFindPatient,
+    dischargePatient,
+    editPatientMedical,
+    getPatientsInDepartment,
+    receptionistFindOnePatient,
+    getAllPatients,
+};
