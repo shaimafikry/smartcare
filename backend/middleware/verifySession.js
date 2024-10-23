95c2209668114d3c36fe9515ee3c0fa91f0c35b1
foash-111 <mr2221@fayoum.edu.eg>
2024-10-23 12:17:24 +0300

    check if the Session is valid and specific to this tab

--- backend/middleware/verifySession.js
+++ backend/middleware/verifySession.js
@@ -1,13 +1,7 @@
-const session = require('express-session');
-const jwt = require('jsonwebtoken');
-const { findUserByEmail, validatePass } = require('../models/user');
-const crypto = require('crypto');
 require('dotenv').config({ path: './config/.env' });
 
-const secret_key = process.env.SECRET_KEY;
 
 const verifySession = ((req, res, next) => {
-	const sessionId = req.cookies['sessionId']; // get the session id 
 	const tabToken = req.headers['tab-token']; // git the tab token
 
 	// Fetch the session from the database or in-memory store
@@ -20,7 +14,6 @@ const verifySession = ((req, res, next) => {
 			res.status(401).json({ message: 'Invalid session' });
 	}
 });
-
 
 
 module.exports = verifySession;
